187d95c1695fa7301236c71c54b44ef440142f47
obed-yaw <obed.agyeman@ashesi.edu.gh>
2021-11-27 15:14:04 +1200

    No major change

--- admin/sidenav.php
+++ admin/sidenav.php
@@ -64,7 +64,12 @@
               <p>Manage User</p>
             </a>
           </li>
-          
+          <li class="nav-item ">
+            <a class="nav-link" href="../logout.php">
+              <i class="material-icons">logout</i>
+              <p>Logout</p>
+            </a>
+          </li>
           <!-- <li class="nav-item active-pro ">
                 <a class="nav-link" href="./upgrade.html">
                     <i class="material-icons">unarchive</i>
